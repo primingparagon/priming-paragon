07dfd7f085564c22648a155c106f6d1fff0442b1
Priming Paragon <primingparagon@gmail.com>
2025-12-08 12:06:32 -0500

    Delete shared-ts/src/pedagogy.ts

--- shared-ts/src/pedagogy.ts
+++
@@ -1,9 +0,0 @@
-export interface TeachingMethod {
-  id: string;
-  name: string;
-  version: string;
-  metadata: Record<string, any>;
-  rawSource?: string;
-  createdBy?: string;
-  createdAt: string;
-}
