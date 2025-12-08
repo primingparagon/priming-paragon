b2bd3e5822afcb991fdf8af093cd8e6e3bab4d2d
Priming Paragon <primingparagon@gmail.com>
2025-12-08 12:31:42 -0500

    Update pedagogy.ts

--- Shared-ts/pedagogy.ts
+++ Shared-ts/pedagogy.ts
@@ -1,1 +1,9 @@
-
+export interface TeachingMethod {
+  id: string;
+  name: string;
+  version: string;
+  metadata: Record<string, any>;
+  rawSource?: string;
+  createdBy?: string;
+  createdAt: string;
+}
