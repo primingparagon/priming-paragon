ec66c4f474cdd76d9f1b8417bc3c295354c826b2
Priming Paragon <primingparagon@gmail.com>
2025-12-13 00:27:01 -0500

    Update user.entity.ts

--- services/auth-service/src/users/user.entity.ts
+++ services/auth-service/src/users/user.entity.ts
@@ -1,1 +1,12 @@
+export interface UserDatabaseRecord {
+  id?: number;
+  email: string;
+  password: string;
+  role?: 'user' | 'admin';
+  created_at?: Date;
+}
+
+export type User = Omit<UserDatabaseRecord, 'password'>;
+
+export type NewUser = Omit<UserDatabaseRecord, 'id' | 'created_at'>;
 
